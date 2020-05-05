09724dd90fc691d838c74de4200a09df6ab65027
Muskan Gupta <muskan.gupta@knoldus.com>
2020-05-05 12:29:44 +0530

    Added some more flows for demonstration

--- src/main/scala/com/knoldus/AkkaDemo.scala
+++ src/main/scala/com/knoldus/AkkaDemo.scala
@@ -2,20 +2,23 @@ package com.knoldus
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, Materializer}
-import akka.stream.scaladsl.Flow
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Keep}
 import com.knoldus.Sinks.sideEffectSink
-import com.knoldus.Sources.{delayBased, numbers}
+import com.knoldus.Sources.{delayBased, numbers, single}
 
-object AkkaDemo extends App{
+object AkkaDemo extends App {
 
   implicit val system: ActorSystem = ActorSystem("Demo")
-  implicit val materializer = Materializer
+  implicit val materializer: Materializer.type = Materializer
 
   //different flow
   val flowToDouble: Flow[Int, Int, NotUsed] = Flow[Int].map(_ * 2)
   val groupTwo: Flow[Int, Seq[Int], NotUsed] = Flow[Int].grouped(2)
-
- numbers.via(flowToDouble).to(sideEffectSink).run()
+  val streamToUpper: Flow[String, String, NotUsed] = Flow[String].map(_.toUpperCase)
+  val streamSum: Flow[Int, Int, NotUsed] = Flow[Int].reduce(_+_)
+  single.viaMat(streamToUpper)(Keep.left).toMat(sideEffectSink)(Keep.left).run()
+  numbers.via(flowToDouble).to(sideEffectSink).run()
+  numbers.via(streamSum).async.runWith(sideEffectSink)
   delayBased.runWith(sideEffectSink)
 }
